b6d346f3d46dd4496a02dbb6229e3c2fbe28c467
Josh Holland <jrh@joshh.co.uk>
2009-07-19 00:17:57 +0100

    Neatened up setup.py
    Changed the horrible looking ' '.join('long string continued over
    several lines including indentation'.split()) to using adjacent string
    concatenation. Looks much nicer.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup, Extension
 
 setup(name = 'suvat',
       version = '0.1.1',
-      description = ' '.join('A simple GUI to solve equations of motion under\
-              constant acceleration'.split()),
-      long_description = ' '.join('suvat uses the equations of motion with\
-              constant acceleration to find the values of displacement,\
-              initial velocity, final velocity, acceleration and time,\
-              given three of them'.split()),
+      description = 'A simple GUI to solve equations of motion under '
+              'constant acceleration',
+      long_description = 'suvat uses the equations of motion with '
+              'constant acceleration to find the values of displacement, '
+              'initial velocity, final velocity, acceleration and time, '
+              'given three of them',
       author = 'Josh Holland',
       author_email = 'jrh@joshh.co.uk',
       packages = ['suvatlib'],
