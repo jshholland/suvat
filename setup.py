7cc48bf2a460b5a8738b827dc4d767e0da815d36
Josh Holland <jrh@joshh.co.uk>
2009-07-28 22:43:58 +0100

    Added .desktop file
    I added a .desktop file from Fabian Affolter, who decided to package
    suvat for Fedora (thanks). I also tweaked setup.py to install it
    properly (probably a better way to do it than what I did). Fabian also
    noticed a bug in the script which I fixed.

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 from distutils.core import setup, Extension
+from distutils.command.build import build as _build
+import os
 
-setup(name = 'suvat',
+class build(_build):
+    def run(self):
+        _build.run(self)
+        os.system("desktop-file-install data/suvat.desktop")
+
+
+setup(cmdclass = {'build': build},
+      name = 'suvat',
       version = '0.2',
       description = 'A simple GUI to solve equations of motion under '
               'constant acceleration',
@@ -16,5 +25,5 @@ setup(name = 'suvat',
       ext_modules = [Extension('suvatext', sources = ['suvatlib/suvatext.c'])],
       requires = ['pygtk (>= 2.0)', 'gtk'],
       license = 'GPL-3',
-      data_files = [('/usr/share/man/man1', 'doc/suvat.1.gz')],
+      data_files = [('/usr/share/man/man1', ['doc/suvat.1.gz'])],
       url = 'https://launchpad.net/suvat')
